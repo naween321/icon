11d4dfcb3281c5dcf5bf86e7fd3fdb2a04bcaf5e
pnabiniw <pnabin@insightworkshop.io>
2022-02-08 14:31:49 +0545

    [update] changes the static location

--- app.py
+++ app.py
@@ -14,19 +14,15 @@ def stat():
     return '<h1>ICON PROJECT</h1>'
 
 
-@app.route('/static/<placeholder>/')
+@app.route('/icon/<placeholder>/')
 def get_icon(placeholder):
-    placeholder = placeholder.upper()[0:2]
-    try:
-        return send_file(f'static/{placeholder}.png', mimetype='image/gif')
-    except:
-        icon = Image.open("static/theme_color.png")
-        edit = ImageDraw.Draw(icon)
-        font = ImageFont.truetype("salmapro.otf", 25)
-        w, h = edit.textsize(placeholder, font=font)
-        edit.text(((50-w)/2, (50-h)/2), placeholder, (255, 255, 255), font=font)
-        icon.save(f'static/{placeholder}.png')
-        return send_file(f'static/{placeholder}.png', mimetype='image/gif')
+    icon = Image.open("static/theme_color.png")
+    edit = ImageDraw.Draw(icon)
+    font = ImageFont.truetype("salmapro.otf", 25)
+    w, h = edit.textsize(placeholder, font=font)
+    edit.text(((50-w)/2, (50-h)/2), placeholder, (255, 255, 255), font=font)
+    icon.save(f'static/{placeholder}.png')
+    return send_file(f'static/{placeholder}.png', mimetype='image/gif')
 
 
 if __name__ == '__main__':
